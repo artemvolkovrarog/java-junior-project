effdc9091c3f435be518ac26bbc0601266a22ade
mech-Maks <maksim_28_11_1999@mail.ru>
2021-09-06 10:55:42 +0300

    Server side logic

---
+++ src/main/java/com/db/edu/server/Server.java
@@ -0,0 +1,32 @@
+package com.db.edu.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try (final ServerSocket listener = new ServerSocket(10_000)) {
+            listener.setSoTimeout(500);
+            try (
+                    final Socket connection = listener.accept();
+                    final DataInputStream input = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
+                    final DataOutputStream output = new DataOutputStream(new BufferedOutputStream(connection.getOutputStream()))
+            ) {
+
+                Controller controller = new Controller(output);
+
+                while (true) {
+                    final String read = input.readUTF();
+                    controller.sendMsg(read);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace(System.err);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+}
