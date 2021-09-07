81e878cd0cf63a2a09e35088e976910ecd20707c
ivanlokhno <ivanlokhno@mail.ru>
2021-09-07 13:20:35 +0300

    test for saver

--- src/test/java/com/db/edu/team01/save/SaverTest.java
+++ src/test/java/com/db/edu/team01/save/SaverTest.java
@@ -9,10 +9,11 @@ import java.util.Arrays;
 import java.util.List;
 
 class SaverTest {
-    private final String fileName = "src/test/java/com/db/edu/team01/save/testmessagebase.txt";
+    private final String fileName = "testmessagebase.txt";
     File source = new File(fileName);
     @Test
     public void saveMessageToFileCorrectly() throws IOException {
+        source.createNewFile();
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(
                         new BufferedInputStream(
@@ -20,9 +21,11 @@ class SaverTest {
         String expected = Decorator.getFormattedStr("Hello", "Name");
         Saver.save("Hello", "Name");
         Assertions.assertEquals(expected, in.readLine());
+        source.delete();
     }
     @Test
     public void getHistoryCorrectly() throws IOException {
+        source.createNewFile();
         String firstName = "firstName";
         String secondName = "secondName";
         String firstHello = "firstHello";
@@ -32,6 +35,7 @@ class SaverTest {
         Saver.save("firstHello", "firstName");
         Saver.save("secondHello", "secondName");
         Assertions.assertEquals(expected, Saver.getHistory());
+        source.delete();
     }
 
 }
