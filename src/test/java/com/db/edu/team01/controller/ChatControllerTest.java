6f0dfe739bed38f55ee0101a3791e183db7f8aa8
Olesia <olesiag07@yandex.ru>
2021-09-07 17:01:54 +0300

    added controller test which handles invalid input

--- src/test/java/com/db/edu/team01/controller/ChatControllerTest.java
+++ src/test/java/com/db/edu/team01/controller/ChatControllerTest.java
@@ -5,17 +5,27 @@ import org.junit.jupiter.api.Test;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class ChatControllerTest {
+    private DataOutputStream outputMock = mock(DataOutputStream.class);
+    private ChatController chatController = new ChatController(outputMock);
+
     @Test
     public void shouldHandleCorrectInput() throws IOException {
         String testInput = "/chid testUser1";
-        DataOutputStream outputMock = mock(DataOutputStream.class);
-        ChatController chatController = new ChatController(outputMock);
-
         chatController.handleInput(testInput);
         verify(outputMock, times(1)).writeUTF("Added user");
         verify(outputMock, times(1)).flush();
     }
+
+    @Test
+    public void shouldNotifyWhenInputCommandIsInvalid() throws IOException {
+        String testInput = "/test testInput";
+        chatController.handleInput(testInput);
+        verify(outputMock, times(1)).writeUTF("Unknown command, try again");
+    }
+
+
 }
